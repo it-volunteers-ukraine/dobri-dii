bbd1d11f46921381726d85ef5f56aae880a9d5c7
M.Mostova <mmainfo15@gmail.com>
2023-10-11 00:51:23 +0300

    Create project

---
+++ templates/gallery.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: gallery
+*/
+get_header();
+?>
+<h2>gallery</h2>
+
+
+
+<?php get_footer(); ?>
---
+++ templates/news.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: news
+*/
+get_header();
+?>
+<h2>news</h2>
+
+
+
+<?php get_footer(); ?>
