76a7c847574e48d3a39de51c80d01238d5195a11
M.Mostova <mmainfo15@gmail.com>
2023-10-13 14:39:02 +0300

    Updated donate section

--- template-parts/donate-section.php
+++ template-parts/donate-section.php
@@ -1,16 +1,14 @@
 <section class="donate-section container">
     <div class="donate-section__img">
-        <img src="<?php bloginfo( 'template_url' ); ?>/assets/images/volunteer.png" alt="">
         <div class="donate-section__help">
             <img src="<?php bloginfo( 'template_url' ); ?>/assets/images/icon_hands.svg" alt="Потиснення рук">
-          <span>Ми допомагаємо тисячам людей завдяки вашим донатам</span>
+          <span><?php the_field( 'info','option' ); ?></span>
         </div>
     </div>
 
-    <div>
-      <h3 class="title-secondary donate-section__title">Кожен Ваш внесок - важливий!</h3>
-      <p class="text-secondary donate-section__text">Ваша фінансова підтримка допоможе нам здійснювати більше добрих дій та реалізовувати важливі проекти для покращення нашої спільноти.</p>
-      <a class="button button--blue donate-section__link " href="#">Підтримати</a>
+    <div class="donate-section__description">
+      <h3 class="donate-section__title"><?php the_field( 'title','option' ); ?></h3>
+      <p class="text-secondary donate-section__text"><?php the_field( 'text','option' ); ?></p>
+      <a class="button button--blue donate-section__link " href="#"><?php the_field( 'button-text','option' ); ?></a>
     </div>
- 
-</section>
+ </section>
