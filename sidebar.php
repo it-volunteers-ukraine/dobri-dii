b536153b6f1ee10c6b6ff776b4d68f279728badb
M.Mostova <mmainfo15@gmail.com>
2023-10-25 21:23:02 +0300

    One-news-page

--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,62 @@
  * @package wp-it-volunteers
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
 ?>
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<aside id="secondary" class="sidebar">
+
+    <h3 class=".title-main sidebar__title">Інші новини</h3>
+
+
+    <ul class="sidebar__list">
+
+        <?php
+            global $post;
+            $myposts = get_posts([ 
+            'post_type' => 'news',  
+            'posts_per_page' => 2,   
+
+            ]);
+
+            if( $myposts ){
+              foreach( $myposts as $post ){
+                setup_postdata( $post );
+                ?>
+        <li class="">
+            <div class="sidebar__img">
+                <img src="<?php the_field('img'); ?>" alt="<?php the_field('alt'); ?>">
+            </div>
+            <div class="sidebar__info">
+                <span class="sidebar__date"><?php the_field('date'); ?></span>
+                <h3 class="title-secondary sidebar__subtitle"><?php the_title(); ?></h3>
+                <a class="button--arrow" href="<?php the_permalink(); ?>">Читати більше
+                    <svg width="24px" height="24px">
+                        <use class="arrow-up"
+                            href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#arrow-up-right">
+                        </use>
+                        <use class="arrow-right"
+                            href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-arrow-right">
+                        </use>
+                    </svg>
+                </a>
+            </div>
+        </li>
+        <?php 
+            }
+            } 
+            wp_reset_postdata(); 
+            ?>
+
+    </ul>
+
+    <a class="button--arrow" href="<?php echo get_permalink( 11); ?>">Усі новини
+        <svg width="24px" height="24px">
+            <use class="arrow-up"
+                href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#arrow-up-right">
+            </use>
+            <use class="arrow-right"
+                href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-arrow-right">
+            </use>
+        </svg>
+    </a>
 </aside><!-- #secondary -->
--- single-news.php
+++ single-news.php
@@ -18,116 +18,26 @@ get_header();
         <span class="one-news__date"><?php the_field('date'); ?></span>
     </section>
 
-
-    <div>
+    <div class="one-news__wrapper">
         <section class="one-news__article">
-            <h3 class="title-secondary one-news__title"><?php the_title(); ?></h3>
-            <p class="text-secondary one-news__text"><?php the_field('text'); ?></p>
-
-
-            <div class="gallery">
-                <div class="swiper swiperOneNews2">
-                    <div class="swiper-wrapper">
-                        <div class=" news-gallery__item2 swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-                        </div>
-
-                        <div class="news-gallery__item2 swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-                        </div>
-
-                        <div class="news-gallery__item2 swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                        </div>
-
-                        <div class="news-gallery__item2 swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="gallery__wrapper">
-                    <div thumbsSlider="" class="swiper swiperOneNews">
-
-                        <div class="news-gallery__container swiper-wrapper">
-
-                            <div class="news-gallery__item  swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-                            </div>
-
-                            <div class="news-gallery__item swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-                            </div>
-
-                            <div class="news-gallery__item  swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                            </div>
-
-                            <div class="news-gallery__item  swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                            </div>
-                        </div>
-                        <div class="news__pagination"></div>
-                    </div>
-
-
-                    <div class="button-prev">
-                        <svg width="24px" height="24px">
-                            <use class="arrow-up"
-                                href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#chevron-left">
-                            </use>
-                        </svg>
-                    </div>
-
-                    <div class="button-next">
-                        <svg width="24px" height="24px">
-                            <use class="arrow-up"
-                                href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#chevron-right">
-                            </use>
-                        </svg>
-                    </div>
-
-                </div>
-
-            </div>
-
-        </section>
-        <section class="sidebar"></section>
-    </div>
-
-
-
-    <!-- <?php
+            <?php
 		while ( have_posts() ) :
-			the_post();
-						    get_template_part( 'template-parts/content', get_post_type() );
-				?>
+		    	the_post();
 
+			    get_template_part( 'template-parts/content', 'news', get_post_type() );
+			
+        endwhile; 
+		    ?>
+        </section>
 
-
-
-    <?php  the_post_navigation(
-    array(
-    'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'wp-it-volunteers' ) . '</span> <span
-        class="nav-title">%title</span>',
-    'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'wp-it-volunteers' ) . '</span> <span
-        class="nav-title">%title</span>',
-    )
-    );
-
-    // If comments are open or we have at least one comment, load up the comment template.
-    // if ( comments_open() || get_comments_number() ) :
-    // comments_template();
-    // endif;
-
-    endwhile; // End of the loop.
-    ?> -->
+        <section class="sidebar">
+            <?php get_sidebar(); ?>
+        </section>
+    </div>
 
 </main><!-- #main -->
 
 <?php get_template_part( 'template-parts/donate-section' ); ?>
 
 <?php
-// get_sidebar();
 get_footer();
