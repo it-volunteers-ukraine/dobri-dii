da7f98eecca4832fd244bc6430ec6d4b6c6b69f2
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-10-28 17:47:09 +0200

    Update carousel navigation at gallery page

--- src/scripts/template-scripts/gallery.js
+++ src/scripts/template-scripts/gallery.js
@@ -5,8 +5,8 @@ const swiperStories = new Swiper(".swiperGallery", {
         clickable: true,
     },
     navigation: {
-        nextEl: ".button-next",
-        prevEl: ".button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 });
 
