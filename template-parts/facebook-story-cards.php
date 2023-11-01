6d5f2c2ed2e36e46c1033093644b4d87256c1d7a
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-11-01 15:03:37 +0100

    Update link for facebook story card at about page

--- template-parts/facebook-story-cards.php
+++ template-parts/facebook-story-cards.php
@@ -2,17 +2,18 @@
 	<?php while ( have_rows( 'gallery_card' ) ): the_row(); ?>
         <article class="swiper-slide stories-card">
             <div class="stories-card-img">
-                <img src="<?php the_sub_field( 'gallery_card_image' ); ?>"
-                     alt="<?php the_sub_field( 'gallery_card_image_alt' ); ?>">
+                <img src="<?php the_sub_field( 'image' ); ?>"
+                     alt="<?php the_sub_field( 'image_alt' ); ?>">
             </div>
             <div class="stories-card-info">
                 <p class="stories-card-text text-secondary">
-					<?php the_sub_field( 'gallery_card_text' ); ?>
+					<?php the_sub_field( 'text' ); ?>
                 </p>
                 <div class="stories-card-bottom">
                     <a class="button--arrow"
-                       href="<?php the_sub_field( 'gallery_card_link' ); ?>">Читати
-                        більше
+                       href="<?php the_sub_field( 'link' ); ?>">
+						<?php the_sub_field( 'link_text' ); ?>
+                        <span><?php the_sub_field( 'link_social' ); ?></span>
                         <svg width="24px" height="24px">
                             <use class="arrow-up"
                                  href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#arrow-up-right"></use>
@@ -20,7 +21,7 @@
                                  href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-arrow-right"></use>
                         </svg>
                     </a>
-                    <span class="stories-card-author"><?php the_sub_field( 'gallery_card_author' ); ?></span>
+                    <span class="stories-card-author"><?php the_sub_field( 'author' ); ?></span>
                 </div>
             </div>
         </article>
