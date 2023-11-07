1773c9315999b530a7f6c3747933160e7e45e556
M.Mostova <mmainfo15@gmail.com>
2023-11-07 23:31:21 +0200

    Updated single-news.php, styles for news page

--- single-news.php
+++ single-news.php
@@ -12,8 +12,11 @@ get_header();
 
 <main class="main container">
 
-    <section class="one-news__hero"
-        style="background: linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%), linear-gradient(180deg, rgba(0, 0, 0, 0.00) 34.9%, rgba(0, 0, 0, 0.14) 52.6%, rgba(0, 0, 0, 0.33) 67.71%, rgba(0, 0, 0, 0.42) 78.65%, rgba(0, 0, 0, 0.60) 89.58%, rgba(0, 0, 0, 0.75) 100%), url(<?php the_field('img'); ?>) no-repeat center top / cover;">
+    <section class="one-news__hero" style="<?php if(get_field('img_bg')) : ?>
+        background: linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%), linear-gradient(180deg, rgba(0, 0, 0, 0.00) 34.9%, rgba(0, 0, 0, 0.14) 52.6%, rgba(0, 0, 0, 0.33) 67.71%, rgba(0, 0, 0, 0.42) 78.65%, rgba(0, 0, 0, 0.60) 89.58%, rgba(0, 0, 0, 0.75) 100%), url(<?php the_field('img_bg'); ?>) no-repeat center top / cover;
+    <?php else : ?>
+        background: linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%), linear-gradient(180deg, rgba(0, 0, 0, 0.00) 34.9%, rgba(0, 0, 0, 0.14) 52.6%, rgba(0, 0, 0, 0.33) 67.71%, rgba(0, 0, 0, 0.42) 78.65%, rgba(0, 0, 0, 0.60) 89.58%, rgba(0, 0, 0, 0.75) 100%), url(<?php the_field('img') ?>) no-repeat center top / cover;
+    <?php endif ?>">
         <h3 class=" title-secondary one-news__title"><?php the_title(); ?></h3>
         <span class="one-news__date"><?php the_field('date'); ?></span>
     </section>
