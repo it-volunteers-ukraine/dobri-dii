c99e842c8de159b63487409e6f1a1611f08fdf2b
M.Mostova <mmainfo15@gmail.com>
2023-11-11 20:10:52 +0200

    Update mobile menu

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -3,23 +3,14 @@
 const menuBtnRef = document.querySelector("[data-menu-button]");
 const mobileMenuRef = document.querySelector("[data-menu]");
 
-const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
 const onToggleMenu = () => {
+  const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
   document.body.classList.toggle("is-open");
   menuBtnRef.classList.toggle("is-open");
   mobileMenuRef.classList.toggle("is-open");
+  menuBtnRef.setAttribute("aria-expanded", String(!expanded));
 };
 
-const onButtonClick = () => {
-  menuBtnRef.setAttribute("aria-expanded", !expanded);
-  onToggleMenu();
-};
-
-const onMenuClick = () => {
-  menuBtnRef.setAttribute("aria-expanded", expanded);
-  onToggleMenu();
-};
-
-menuBtnRef.addEventListener("click", onButtonClick);
-mobileMenuRef.addEventListener("click", onMenuClick);
+menuBtnRef.addEventListener("click", onToggleMenu);
+mobileMenuRef.addEventListener("click", onToggleMenu);
