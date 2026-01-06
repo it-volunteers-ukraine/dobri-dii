fa9150528954f9a24602f545de74e37603c38f63
olya <katniss30051991@gmail.com>
2026-01-06 23:04:27 +0200

    start develope videotek template

--- templates/gallery.php
+++ templates/gallery.php
@@ -9,7 +9,7 @@
     <div class="container">
         <h1 class="title-main"><?php the_title(); ?></h1>
 
-		<?php
+        <?php
 			$args          = ( [
 				'fields'         => 'ids',
 				'posts_per_page' => 3,
@@ -25,52 +25,50 @@
 					$gallery_query->the_post();
 					?>
 
-                    <article class="gallery-card">
-                        <div class="image-gallery">
-                            <h3 class="title-secondary title-mobile"><?php the_title(); ?></h3>
-                            <div class="swiper swiperGallery">
-                                <div class="swiper-wrapper">
-									<?php if ( have_rows( 'gallery' ) ): ?>
-										<?php while ( have_rows( 'gallery' ) ): the_row(); ?>
-                                            <div class="swiper-slide">
-                                                <div class="image-wrapper">
-                                                    <a href="<?php the_sub_field( 'img' ); ?>"
-                                                       data-lightbox="<?php the_ID(); ?>">
-                                                        <img src="<?php the_sub_field( 'img' ); ?>"
-                                                             alt="<?php the_sub_field( 'alt' ); ?>">
-                                                    </a>
-                                                </div>
-                                            </div>
-										<?php endwhile; ?>
-									<?php endif; ?>
-                                </div>
-                                <div class="swiper-pagination"></div>
-                                <div class="swiper-button-next"></div>
-                                <div class="swiper-button-prev"></div>
+        <article class="gallery-card">
+            <div class="image-gallery">
+                <h3 class="title-secondary title-mobile"><?php the_title(); ?></h3>
+                <div class="swiper swiperGallery">
+                    <div class="swiper-wrapper">
+                        <?php if ( have_rows( 'gallery' ) ): ?>
+                        <?php while ( have_rows( 'gallery' ) ): the_row(); ?>
+                        <div class="swiper-slide">
+                            <div class="image-wrapper">
+                                <a href="<?php the_sub_field( 'img' ); ?>" data-lightbox="<?php the_ID(); ?>">
+                                    <img src="<?php the_sub_field( 'img' ); ?>" alt="<?php the_sub_field( 'alt' ); ?>">
+                                </a>
                             </div>
                         </div>
+                        <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+                    <div class="swiper-pagination"></div>
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                </div>
+            </div>
 
-                        <div>
-                            <div class="date-desktop">
-                                <img src="<?php bloginfo( 'template_url' ); ?>/assets/images/date-line.svg" alt="#">
-                                <span><?php the_field( 'date' ); ?></span>
-                            </div>
-                            <h3 class="title-secondary title-desktop"><?php the_title(); ?></h3>
-                            <div class="date-mobile">
-                                <img src="<?php bloginfo( 'template_url' ); ?>/assets/images/date-line.svg" alt="#">
-                                <span><?php the_field( 'date' ); ?></span>
-                            </div>
-                            <p class="text-main"><?php the_field( 'text' ); ?></p>
-                        </div>
-                    </article>
+            <div>
+                <div class="date-desktop">
+                    <img src="<?php bloginfo( 'template_url' ); ?>/assets/images/date-line.svg" alt="#">
+                    <span><?php the_field( 'date' ); ?></span>
+                </div>
+                <h3 class="title-secondary title-desktop"><?php the_title(); ?></h3>
+                <div class="date-mobile">
+                    <img src="<?php bloginfo( 'template_url' ); ?>/assets/images/date-line.svg" alt="#">
+                    <span><?php the_field( 'date' ); ?></span>
+                </div>
+                <p class="text-main"><?php the_field( 'text' ); ?></p>
+            </div>
+        </article>
 
-					<?php
+        <?php
 				}
 			}
 		?>
 
         <div class="pagination">
-			<?php
+            <?php
 				echo paginate_links( [
 					'base'      => get_pagenum_link( 1 ) . '%_%',
 					'format'    => '/page/%#%',
@@ -83,14 +81,9 @@
         </div>
     </div>
 
-	<?php get_template_part( 'template-parts/donate-section' ); ?>
+    <?php get_template_part( 'template-parts/donate-section' ); ?>
 
 </main>
 
 
 <?php get_footer(); ?>
-
-
-
-
-
--- templates/videotek.php
+++ templates/videotek.php
@@ -6,8 +6,58 @@
 	get_header();
 ?>
 
-<main>
+<main class="main">
+    <section class="container">
+        <h1 class="title-main"><?php the_title(); ?></h1>
 
+        <?php
+if( get_query_var('page') ) {
+  $page = get_query_var( 'page' );
+} else {
+  $page = 1;
+}
+
+$row              = 0;
+$videos_per_page  = 6; 
+$videos           = get_field( 'video_list' );
+$total            = count( $videos );
+$pages            = ceil( $total / $videos_per_page );
+$min              = ( ( $page * $videos_per_page ) - $videos_per_page ) + 1;
+$max              = ( $min + $videos_per_page ) - 1;
+
+if( have_rows( 'video_list' ) ) : ?>
+        <?php while( have_rows( 'video_list' ) ): the_row();
+
+    $row++;
+
+    if($row < $min) { continue; }
+
+    if($row > $max) { break; } ?>
+
+        <?php $thumbnail_image = get_sub_field( 'thumbnail_image' ); ?>
+        <?php $video_link = get_sub_field( 'video_link' ); ?>
+        <a href="<?php echo $video_link; ?>" data-lightbox="roadtrip">
+            <img src="<?php echo $thumbnail_image['sizes']['thumbnail']; ?>" alt="">
+        </a>
+
+        <?php endwhile;
+
+  // Pagination
+  echo paginate_links( array(
+    'base' => get_permalink() . '%#%' . '/',
+    'format' => '?page=%#%',
+    'current' => $page,
+    'total' => $pages
+  ) );
+  ?>
+
+        <?php else: ?>
+
+        No images found
+
+        <?php endif; ?>
+
+    </section>
 </main>
 
 <?php get_footer(); ?>
