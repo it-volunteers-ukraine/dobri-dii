dfe821ca483f719e06262c020645e5f1f0fd6f94
Bukina O <katniss30051991@gmail.com>
2026-05-03 21:05:54 +0300

    Add checking

--- template-parts/feedback-section.php
+++ template-parts/feedback-section.php
@@ -4,32 +4,45 @@ $feedback_title = get_field('feedback_title', 'option');
 $feedback_img = get_field('feedback_img', 'option');
 $form_id = get_field('feedback_form_id', 'option'); ?>
 
-<section class="feedback-section container">
-    <h3 class="title"><?php echo $feedback_title; ?></h3>
-    <div class="section-columns">
-        <div class="left-column">
-            <div class="column-content">
-                <?php if ($feedback_info): ?>
-                    <div class="info-content">
-                        <div class="icon-wrapper">
-                            <img src="<?php bloginfo('template_url'); ?>/assets/images/note.svg" alt="">
+<?php if ($form_id): ?>
+    <section class="feedback-section container">
+        <?php if ($feedback_title): ?>
+            <h3 class="title"><?php echo $feedback_title; ?></h3>
+        <?php endif; ?>
+
+        <?php if ($feedback_info || $feedback_img || $form_id): ?>
+            <div class="section-columns">
+                <?php if ($feedback_info || $feedback_img): ?>
+                    <div class="left-column">
+                        <div class="column-content">
+                            <?php if ($feedback_info): ?>
+                                <div class="info-content">
+                                    <div class="icon-wrapper">
+                                        <img src="<?php bloginfo('template_url'); ?>/assets/images/note.svg" alt="">
+                                    </div>
+                                    <p class="text"><?php echo $feedback_info; ?></p>
+                                </div>
+                            <?php endif; ?>
+
+                            <?php if ($feedback_img): ?>
+                                <div class="section-image">
+                                    <img src="<?php echo $feedback_img; ?>" alt="Волонтер в роботі">
+                                </div>
+                            <?php endif; ?>
                         </div>
-                        <p class="text"><?php echo $feedback_info; ?></p>
                     </div>
                 <?php endif; ?>
 
-                <?php if (!empty($feedback_img)): ?>
-                    <div class="section-image">
-                        <img src="<?php echo $feedback_img; ?>" alt="Волонтер в роботі">
+
+                <?php if ($form_id): ?>
+                    <div class="right-column">
+                        <?php echo do_shortcode('[contact-form-7 id="' . $form_id . '" title="false" ajax="true"]');
+                        ?>
                     </div>
                 <?php endif; ?>
             </div>
-        </div>
+        <?php endif; ?>
 
-        <div class="right-column">
-            <?php echo do_shortcode('[contact-form-7 id="' . $form_id . '" title="false" ajax="true"]');
-            ?>
-        </div>
-    </div>
 
-</section>
+    </section>
+<?php endif; ?>
